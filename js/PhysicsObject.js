7c5f84f1f17cb52198caaa6acfa8818b8a725069
carlos valera <cookyt2@gmail.com>
2012-10-07 03:47:38 -0400

    propogated defaults to physics object constructors

--- js/PhysicsObject.js
+++ js/PhysicsObject.js
@@ -1,18 +1,17 @@
 function PhysicsObject() { }
 
-PhysicsObject.prototype.initiallize = function(shape)
+PhysicsObject.prototype.initiallize = function(shape, defaults)
 {
     this.shape = shape;
     this.shape.physics = this;
 
     this.position = shape.bounds.center;
-    this.velocity = new paper.Point(0, 0);
+    this.velocity = defaults.velocity.clone();
+    this.angular_vel = defaults.angular_vel;
 
-    this.angular_vel = 0;
-
-    this.mass = 0;
-    this.restitution = 1;
-    this.is_static = false;
+    this.mass = defaults.mass;
+    this.restitution = defaults.restitution;
+    this.is_static = defaults.is_static;
 
     this.grabbed = false;
 }
@@ -27,21 +26,22 @@ PhysicsObject.prototype.step = function(delta)
     this.shape.rotate(this.angular_vel*delta);
 };
 
-function PhysicsBox(top_left, bot_right)
+function PhysicsBox(top_left, bot_right, defaults)
 {
     var shape = new paper.Path.Rectangle(top_left, bot_right);
-    shape.fillColor = 'black';
-    this.initiallize(shape);
+    shape.fillColor = defaults.fill_color;
+    this.initiallize(shape, defaults);
 }
 PhysicsBox.prototype = new PhysicsObject();
 PhysicsBox.prototype.constructor = PhysicsBox;
 
-//Creates a ball with center at given position and with given radius
-function PhysicsBall(position, radius)
+//Creates a ball with center at given position and with given
+//radius
+function PhysicsBall(position, radius, defaults)
 {
     var shape = new paper.Path.Circle(position, radius);
-    shape.fillColor = 'black';
-    this.initiallize(shape);
+    shape.fillColor = defaults.fill_color;
+    this.initiallize(shape, defaults);
 }
 PhysicsBall.prototype = new PhysicsObject();
 PhysicsBall.prototype.constructor = PhysicsBall;
