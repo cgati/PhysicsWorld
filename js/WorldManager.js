e9f19c1f565ccb0d3cf318abf5123208ef0fbea2
carlos valera <cookyt2@gmail.com>
2012-10-06 23:41:33 -0400

    fixed grab while moving bug

--- js/WorldManager.js
+++ js/WorldManager.js
@@ -1,17 +1,31 @@
 function WorldManager()
 {
     this.objects = new Array();
-    this.gravity = new paper.Point(0,10);
+    this.gravity = new paper.Point(0,2);
+    this.paused = false;
 }
 
 WorldManager.prototype.step = function(event)
 {
-    var len = this.objects.length;
-    for (i=0; i<len; i++)
+    if (!this.paused)
     {
-        this.objects[i].step(event.delta);
-        this.objects[i].velocity = this.objects[i].velocity.add(this.gravity);
+        var len = this.objects.length;
+        for (i=0; i<len; i++)
+        {
+            this.objects[i].step(event.delta);
+            this.objects[i].velocity = this.objects[i].velocity.add(this.gravity);
+        }
     }
+}
+
+WorldManager.prototype.pause = function()
+{
+    this.paused = true;
+}
+
+WorldManager.prototype.play = function()
+{
+    this.paused = false;
 }
 
 WorldManager.prototype.addBall = function(position, radius)
