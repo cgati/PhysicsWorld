49263bf787c74b24345fcb6556d9559934a6d1cc
carlos valera <cookyt2@gmail.com>
2012-10-06 23:14:13 -0400

    Added gravity.

--- js/PhysicsObject.js
+++ js/PhysicsObject.js
@@ -1,32 +1,32 @@
-function PhysicsObject(position)
-{
-    this.position = position;
-    this.velocity = new Point(0, 0);
+function PhysicsObject() { }
 
-    this.rotation = 0;
-    this.rot_vel = 0;
+PhysicsObject.prototype.initiallize = function(shape)
+{
+    this.shape = shape;
+    this.shape.physics = this;
+
+    this.position = shape.bounds.center;
+    this.velocity = new paper.Point(0, 0);
+
+    this.angular_vel = 0;
 
     this.mass = 0;
     this.restitution = 1;
     this.is_static = false;
-
-    this.shape = null;
 }
 
-PhysicsObject.prototype.step = function(dt)
+PhysicsObject.prototype.step = function(delta)
 {
-    this.position.add(velocity.normalize(velocity.length*dt));
-    this.shape.moveTo(position);
-    this.rotation += rot_vel*dt;
-}
+    this.position = this.position.add(this.velocity.normalize(this.velocity.length*delta));
+    this.shape.position = this.position;
+    this.shape.rotate(this.angular_vel*delta);
+};
 
 function PhysicsBox(top_left, bot_right)
 {
-    var position = new paper.Point((top_left.x + bot_right.x)/2, 
-                                   (top_left.y + bot_right.y)/2);
-    PhysicsObject.apply(this, [position]);
-    this.shape = new paper.Path.Rectangle(top_left, bot_right);
-    this.shape.fillColor = 'black';
+    var shape = new paper.Path.Rectangle(top_left, bot_right);
+    shape.fillColor = 'black';
+    this.initiallize(shape);
 }
 PhysicsBox.prototype = new PhysicsObject();
 PhysicsBox.prototype.constructor = PhysicsBox;
@@ -34,9 +34,9 @@ PhysicsBox.prototype.constructor = PhysicsBox;
 //Creates a ball with center at given position and with given radius
 function PhysicsBall(position, radius)
 {
-    PhysicsObject.apply(this, [position]);
-    this.shape = new paper.Path.Circle(position, radius);
-    this.shape.fillColor = 'black';
+    var shape = new paper.Path.Circle(position, radius);
+    shape.fillColor = 'black';
+    this.initiallize(shape);
 }
 PhysicsBall.prototype = new PhysicsObject();
 PhysicsBall.prototype.constructor = PhysicsBall;
--- js/WorldManager.js
+++ js/WorldManager.js
@@ -1,13 +1,17 @@
 function WorldManager()
 {
-    this.objects = [];
-    this.dt = 0.1;
+    this.objects = new Array();
+    this.gravity = new paper.Point(0,10);
 }
 
-WorldManager.prototype.step = function()
+WorldManager.prototype.step = function(event)
 {
-    for (obj in this.objects)
-        obj.step(this.dt);
+    var len = this.objects.length;
+    for (i=0; i<len; i++)
+    {
+        this.objects[i].step(event.delta);
+        this.objects[i].velocity = this.objects[i].velocity.add(this.gravity);
+    }
 }
 
 WorldManager.prototype.addBall = function(position, radius)
